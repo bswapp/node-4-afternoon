5592b7e32e55d3a716ffbfe80ab460f6672afbf3
bswapp <brookeswapp@gmail.com>
2019-12-31 15:29:55 -0700

    finished

---
+++ build/precache-manifest.b55954c19ddbbf8c510781a8c81bdeff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2397cccadddc3abf5f45e6e4b1b04b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c2be6f386b093a2cfb8",
+    "url": "/static/css/main.3a7460b6.chunk.css"
+  },
+  {
+    "revision": "686c4e4d8c9dc9a5cd3c",
+    "url": "/static/js/2.9424dff7.chunk.js"
+  },
+  {
+    "revision": "10aa99c5b6d4cec368374b3553de7b5b",
+    "url": "/static/js/2.9424dff7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c2be6f386b093a2cfb8",
+    "url": "/static/js/main.d2c57866.chunk.js"
+  },
+  {
+    "revision": "50192f40d45142070df2",
+    "url": "/static/js/runtime-main.0204ecd2.js"
+  }
+]);
